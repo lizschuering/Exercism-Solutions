917b16aaba1c901950c07c4074a2dcb6e4b0843e
Liz Schuering <lschuering@.gmail.com>
2020-10-03 10:10:20 -0500

    updated solution

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,20 +1,14 @@
 def convert(number):
-    if (number % 3 == 0 and number % 5 == 0 and number % 7 == 0):
-        return 'PlingPlangPlong'
-    elif (number % 3 == 0 and number % 5 == 0):
-        return 'PlingPlang'
-    elif (number % 5 == 0 and number % 7 == 0):
-        return 'PlangPlong'
-    elif (number % 3 == 0 and number % 7 == 0):
-        return 'PlingPlong'
-    elif (number % 3 == 0):
-        return 'Pling'
-    elif (number % 5 == 0):
-        return 'Plang'
-    elif (number % 7 == 0):
-        return 'Plong'
-    else:
-        return str(number)
+    raindrop = ""
+    if number % 3 == 0:
+        raindrop += 'Pling'
+    if number % 5 == 0:
+        raindrop += 'Plang'
+    if number % 7 == 0:
+        raindrop += 'Plong'
+    if (number % 3 != 0 and number % 5 != 0 and number % 7 != 0):
+        raindrop = str(number)
+    return raindrop
 
 
-print(convert(3))
+print(convert(5))
